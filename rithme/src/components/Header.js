79fecb82c09fa08b1c72b5f0ee3e8d602da65671
Mawi <maria_mst_92@hotmail.com>
2020-03-14 10:59:37 +0100

    new component nd styles

---
+++ rithme/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <header className="b-header">
+        <div></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
