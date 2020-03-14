4e0c8718b5ac1ca003ae9b41c23845cfdcaa4034
RAUL <raul.sanchez@gmail.com>
2020-03-14 11:04:42 +0100

    primerosppasos

--- rithme/src/components/Bienvenido.js
+++ rithme/src/components/Bienvenido.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logog from './assets/icons/conversationMinor.png';
+import logom from './assets/icons/profileMajorMonotone.png';
+import logop from './assets/icons/group.png';
 
-function App() {
+
+function Bienvenido() {
   return (
     <div className="">
       <header className="">
-      <p>¡Bienvenido!</p>
+      <p className="parrafo">¡Bienvenido!</p>
       </header>
       <div className="c__img">
-          <img src="./assets/icons/profileMajorMonotone.png" className="logo_g"/>
-          <img src="" className="logo_p"/>
-          <img src="" className="logo_p"/>
+          <img src={logog} className="logo_g"/>
+          <img src={logom} className="logo_p"/>
+          <img src={logop} className="logo_p"/>
 
-      </div>
+      </div>""
+      <nav className="c__nav">
+      <a href="">Pefil</a>
+      <a href="">Fan Store</a>
+      <a href="">Tours & Tickets</a>
+      <a href="">Fan Wall</a>
+      <a href="">Amigos y mensajes</a>
+      </nav>
     </div>
   );
 }
 
-export default App;
+export default Bienvenido;
