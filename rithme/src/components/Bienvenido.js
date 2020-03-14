86bfb3bdbd92628d4e7ac3283ecdcda475a0004d
RAUL <raul.sanchez@gmail.com>
2020-03-14 10:31:53 +0100

    nueva rama

---
+++ rithme/src/components/Bienvenido.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+      <p>¡Bienvenido!</p>
+      </header>
+      <div className="c__img">
+          <img src="./assets/icons/profileMajorMonotone.png" className="logo_g"/>
+          <img src="" className="logo_p"/>
+          <img src="" className="logo_p"/>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
