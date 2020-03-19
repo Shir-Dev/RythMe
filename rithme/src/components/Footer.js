34e6dcd7dc71ff4d44a88972c38742d9b6c4b23f
Mawi <maria_mst_92@hotmail.com>
2020-03-19 11:58:14 +0100

    footer hecho

--- rithme/src/components/Footer.js
+++ rithme/src/components/Footer.js
@@ -1,30 +1,15 @@
 import React from "react";
 import "./assets/styles/footer.css";
-import ticket1 from "./assets/icons/ticket1.png";
-import lupa1 from "./assets/icons/lupa1.png";
-import star from "./assets/icons/star.png";
-import campana from "./assets/icons/campana.png";
-import config from "./assets/icons/config.png";
+import home from "./assets/img/home.png";
+import configuraacion from "./assets/img/configuraacion.png";
 
 function Footer() {
   return (
     <div className="container">
       <footer className="b-footer">
-        <picture class="iconos">
-          <img src={ticket1} className="ticket"></img>
-          <img src={lupa1} className="lupa"></img>
-          <img src={star} className="star"></img>
-          <img src={campana} className="campana"></img>
-          <img src={config} className="config"></img>
-        </picture>
+        <img src={configuraacion} className="configuracion"></img>
 
-        <div class="letras">
-          <p class="entradas">Entradas</p>
-          <p class="buscar">Buscar</p>
-          <p class="home">Home</p>
-          <p class="notif">Notificaciones</p>
-          <p class="configu">Configuración</p>
-        </div>
+        <img src={home} className="configuracion"></img>
       </footer>
     </div>
   );
