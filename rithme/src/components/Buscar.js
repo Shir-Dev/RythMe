a0077630982d538c4903d85fd38d2a7ad04e46c7
RAUL <raul.sanchez@gmail.com>
2020-03-27 20:33:33 +0100

    empezada la pag de buscar

--- rithme/src/components/Buscar.js
+++ rithme/src/components/Buscar.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import "./assets/styles/buscar.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import logo_lupa from "./assets/icons/lupa.png";
 
 function Buscar() {
-  const [music, setMusic] = useState([]);
+  const [genres, setGenres] = useState([]);
   useEffect(() => {
     axios
       .get(
@@ -14,13 +15,31 @@ function Buscar() {
       )
       .then(res => {
         console.log(res.data);
-        setMusic(res.data);
+        setGenres(res.data);
       });
   }, []);
+
+  const losGenres = [];
+
+  for (let i = 0; i < genres.length; i++) {
+    const genre = genres[i];
+    losGenres.push(<option value={genre.name}>{genre.name}</option>);
+
+  }
   return (
     <div className="contenedor_g">
       <Header />
-
+      <form>
+    <div class="box">
+    <div class="container-4">
+    <input type="search" id="search" placeholder="Search..." />
+    <button class="icon"><img src={logo_lupa} className="logo_lupa" /></button>
+    </div>
+    </div>
+    <select className="selector">
+    {losGenres}
+    </select>
+    </form>
       <Footer changeNav="buscar" />
     </div>
   );
