bd8f11323f8da1c8f6d87c8ce1653771f7234704
RAUL <raul.sanchez@gmail.com>
2020-03-27 19:30:08 +0100

    pantalla login y empieze de la de busqueda

--- rithme/src/components/Buscar.js
+++ rithme/src/components/Buscar.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./assets/styles/buscar.css";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function Buscar() {
+  const [music, setMusic] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:3333/musicgenres` &&
+          `http://192.168.1.66:3333/musicgenres`
+      )
+      .then(res => {
+        console.log(res.data);
+        setMusic(res.data);
+      });
+  }, []);
   return (
     <div className="contenedor_g">
       <Header />
