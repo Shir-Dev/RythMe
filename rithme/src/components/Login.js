939b2d999256495a67d8ccc36379e069e6a43d71
Mawi <maria_mst_92@hotmail.com>
2020-03-30 11:41:38 +0200

    token front

--- rithme/src/components/Login.js
+++ rithme/src/components/Login.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./assets/styles/login.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Link } from "react-router-dom";
 
-function Login() {
+function Login(props) {
+  const [loginObject, setLoginObject] = useState();
+
+  function autoLogin() {
+    fetch("http://localhost:3333/users/signin", {
+      method: "POST",
+      body: JSON.stringify(loginObject),
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          console.log("usuario logeado");
+        } else {
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        alert("Usuario o contraseña incorrecta.Inténtelo de nuevo.");
+      });
+  }
   const formHeader = {
     headerText: "Login",
     srcArrow: "/"
@@ -24,6 +48,12 @@ function Login() {
             placeholder="Nombre de Usuario"
             name="username"
             id="username"
+            onChange={$event =>
+              setLoginObject({
+                ...loginObject,
+                username: $event.target.value
+              })
+            }
             required
           />
 
@@ -32,17 +62,26 @@ function Login() {
             placeholder="Contraseña"
             name="password"
             id="password"
+            onChange={$event =>
+              setLoginObject({
+                ...loginObject,
+                password: $event.target.value
+              })
+            }
             required
           />
           <input
             className="btnContinue"
-            type="submit"
+            type="button"
             value="Continuar"
             name="continue"
             id="continue"
+            onClick={autoLogin}
           />
         </form>
-        <Link to="/form" className="registro">Registrarme</Link>
+        <Link to="/form" className="registro">
+          Registrarme
+        </Link>
       </div>
     </div>
   );
