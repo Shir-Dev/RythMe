e4a664d4cc4dbd284ff69709ec8ec7abeefcb4ba
Mawi <maria_mst_92@hotmail.com>
2020-03-30 10:57:22 +0200

    header y login

--- rithme/src/components/Login.js
+++ rithme/src/components/Login.js
@@ -4,40 +4,43 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 function Login() {
+  const formHeader = {
+    headerText: "Login",
+    srcArrow: "/"
+  };
   return (
     <div className="contenedor_g">
-      <Header />
+      <Header headerObject={formHeader} />
       <div className="contenedor_login">
-     
-      <form
-        className="contenedor_login"
-        method="post"
-        action="http://localhost:3333/users/signup"
-      >
-         <h2 className="h2">Iniciar Sesion</h2>
-        <input
-          type="text"
-          placeholder="Nombre de Usuario"
-          name="username"
-          id="username"
-          required
-        />
-       
-        <input
-          type="password"
-          placeholder="Contraseña"
-          name="password"
-          id="password"
-          required
-        />
-        <input
-          className="btnContinue"
-          type="submit"
-          value="Continuar"
-          name="continue"
-          id="continue"
-        />
-      </form>
+        <form
+          className="contenedor_login"
+          method="post"
+          action="http://localhost:3333/users/signup"
+        >
+          <h2 className="h2">Iniciar Sesion</h2>
+          <input
+            type="text"
+            placeholder="Nombre de Usuario"
+            name="username"
+            id="username"
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Contraseña"
+            name="password"
+            id="password"
+            required
+          />
+          <input
+            className="btnContinue"
+            type="submit"
+            value="Continuar"
+            name="continue"
+            id="continue"
+          />
+        </form>
       </div>
     </div>
   );
